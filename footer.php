49dd0106d4defb0572860b263474cfbe5789f7da
Hakurei Reimu <166970841+reimu59549@users.noreply.github.com>
2024-11-16 16:49:25 +0900

    highlight.jsの読み込みをCDNからの読み込みに修正、フッターにサイト名とキャッチフレーズが表示されない不具合を修正

--- footer.php
+++ footer.php
@@ -1,11 +1,12 @@
-<?php bloginfo('name'); ?>
-<?php bloginfo('description'); ?>
+<?php echo bloginfo('name'); ?>
+<?php echo bloginfo('description'); ?>
 <p>&copy; <?php echo date("Y"); ?> <?php bloginfo("name"); ?> Theme by <?php $theme_data = wp_get_theme();
 $author = $theme_data->get( 'Author' );
 echo $author; ?>.</p>
 </div>
-<script src="<?php echo get_template_directory_uri(); ?>/js/nav.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/highlight/highlight.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/highlight.min.js"></script>
+<!-- and it's easy to individually load additional languages -->
+<script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/languages/go.min.js"></script>
 <script>hljs.highlightAll();</script>
 </body>
 </html>
