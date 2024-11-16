64ffb8c010f8c2a0f86575a5cbd944b17bccb423
Hakurei Reimu <166970841+reimu59549@users.noreply.github.com>
2024-11-16 18:10:09 +0900

    JavaScriptの読み込みをfunctions-theme-setup.phpで行うように変更(wp_enqueue_script)

--- footer.php
+++ footer.php
@@ -4,9 +4,6 @@
 $author = $theme_data->get( 'Author' );
 echo $author; ?>.</p>
 </div>
-<script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/highlight.min.js"></script>
-<!-- and it's easy to individually load additional languages -->
-<script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/languages/go.min.js"></script>
 <script>hljs.highlightAll();</script>
 </body>
 </html>
