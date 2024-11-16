79499738b7015605e055efece8769f6a3e0cabe8
Hakurei Reimu <166970841+reimu59549@users.noreply.github.com>
2024-11-16 22:46:53 +0900

    トップに戻るボタンを追加

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
+<button class="js-backToTop pagetop"><i class="bi bi-arrow-up"></i></button>
 <?php echo bloginfo('name'); ?>
 <?php echo bloginfo('description'); ?>
 <p>&copy; <?php echo date("Y"); ?> <?php bloginfo("name"); ?> Theme by <?php $theme_data = wp_get_theme();
 $author = $theme_data->get( 'Author' );
 echo $author; ?>.</p>
 </div>
+<script src="<?php echo get_template_directory_uri(); ?>/js/back-to-top.js?ver=1.0.2" type="text/javascript"></script>
 <script>hljs.highlightAll();</script>
 </body>
 </html>
